f4f5b34b35b90e930f92c04e5298a33be5f5ede9
Marcin Fedorowicz <122787758+MarcinFed@users.noreply.github.com>
2024-04-18 19:52:11 +0200

    Update WebSocketConfiguration.java

--- backend/tic_tac_toe/src/main/java/com/Lista1/config/WebSocketConfiguration.java
+++ backend/tic_tac_toe/src/main/java/com/Lista1/config/WebSocketConfiguration.java
@@ -14,7 +14,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gameplay")
-                .setAllowedOrigins("http://ec2-54-210-96-73.compute-1.amazonaws.com", "http://54.210.96.73")
+                .setAllowedOrigins("http://ec2-18-208-140-124.compute-1.amazonaws.com", "http://18.208.140.124")
                 .withSockJS();
     }
 
